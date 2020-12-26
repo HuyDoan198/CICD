c72006015aa8d0ec8f15f9792619fb7bfd0a6281
Huy Doan <huydoan198@gmail.com>
2020-12-27 02:22:12 +0700

    open all

--- Triangle.test.js
+++ Triangle.test.js
@@ -11,29 +11,29 @@ const p7 = new Point(-2,0);
 const p8 = new Point(0,5);
 
 
-// //tam giac thuong => True
-// const tg = new Triangle(p1,p2,p3);
-// test('Kiểm tra tam giác thường', () => {
-//     expect(tg.loaiTamGiac()).toBe("Tam giác thường");
-// })
+//tam giac thuong => True
+const tg = new Triangle(p1,p2,p3);
+test('Kiểm tra tam giác thường', () => {
+    expect(tg.loaiTamGiac()).toBe("Tam giác thường");
+})
 
-// // tam giac vuong => Fail
-// const tg2 = new Triangle(p1,p2,p3);
-// test('Kiểm tra tam giác vuông', () => {
-//     expect(tg2.loaiTamGiac()).toBe("Tam giác vuông");
-// })
+// tam giac vuong => Fail
+const tg2 = new Triangle(p1,p2,p3);
+test('Kiểm tra tam giác vuông', () => {
+    expect(tg2.loaiTamGiac()).toBe("Tam giác vuông");
+})
 
-// // tam giac can => True
-// const tg3 = new Triangle(p6,p7,p8);
-// test('Kiểm tra tam giác cân ?', () => {
-//     expect(tg3.loaiTamGiac()).toBe("Tam giác cân");
-// })
+// tam giac can => True
+const tg3 = new Triangle(p6,p7,p8);
+test('Kiểm tra tam giác cân ?', () => {
+    expect(tg3.loaiTamGiac()).toBe("Tam giác cân");
+})
 
-// // tam giac deu => Fail
-// const tg4 = new Triangle(p6,p7,p8);
-// test('Kiểm tra tam giác cân'?, () => {
-//     expect(tg4.loaiTamGiac()).toBe("Tam giác đều");
-// })
+// tam giac deu => Fail
+const tg4 = new Triangle(p6,p7,p8);
+test('Kiểm tra tam giác cân ?', () => {
+    expect(tg4.loaiTamGiac()).toBe("Tam giác đều");
+})
 
 // tam giac vuông cân => Fail
 const tg5 = new Triangle(p4,p5,p6);
