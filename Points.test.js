74224c1eb70091315f16422bd9fef473d262a1f0
Huy Doan <huydoan198@gmail.com>
2020-12-27 11:09:05 +0700

    kfv

--- Points.test.js
+++ Points.test.js
@@ -9,7 +9,7 @@ test("Is distance between 2 point 3.1622776601683795 ?", () => {
   expect(Point.distance(p1, p2)).toBe(3.1622776601683795);
 });
 
-// //Test tính khoag2 cách 2 điểm => Fail
-// test("Is distance between 2 point 3 ?", () => {
-//   expect(Point.distance(p1, p2)).toBe(3);
-// });
+//Test tính khoag2 cách 2 điểm => Fail
+test("Is distance between 2 point 3 ?", () => {
+  expect(Point.distance(p1, p2)).toBe(3);
+});
