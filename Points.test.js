d566431aedfe673d442a1811247a2503a3783755
Huy Doan <huydoan198@gmail.com>
2020-12-28 21:02:05 +0700

    true code

--- Points.test.js
+++ Points.test.js
@@ -12,15 +12,15 @@ test("Is distance between 2 point 3.1622776601683795 ?", () => {
   expect(Point.distance(p1, p2)).toBe(3.1622776601683795);
 });
 
-//Test tính khoag2 cách 2 điểm => Fail
-test("Is distance between 2 point 3 ?", () => {
-  expect(Point.distance(p1, p2)).toBe(3);
-});
+// //Test tính khoag2 cách 2 điểm => Fail
+// test("Is distance between 2 point 3 ?", () => {
+//   expect(Point.distance(p1, p2)).toBe(3);
+// });
 
-//Test tính khoag2 cách 2 điểm => Fail
-test("calculate distance between 2 point p1,p3", () => {
-  expect(Point.distance(p1, p3)).toBe(5);
-});
+// //Test tính khoag2 cách 2 điểm => Fail
+// test("calculate distance between 2 point p1,p3", () => {
+//   expect(Point.distance(p1, p3)).toBe(5);
+// });
 
 // const p4 = new Point(a,b);
 // const p5 = new Point(c,d);
